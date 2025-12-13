fc00b7f6a9ddbc154f54675dd7f8abbf8b284084
Abirami R <auabi14@gmail.com>
2025-12-13 15:15:04 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Address{
+public static void main(String[]args)
+{
+    Scanner sc = new Scanner(System.in);
+    String mess = sc.nextLine();
+    System.out.println(mess+"\n");
+}
+}
---
+++ Time.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+    public class Time
+    {
+    public static void main(String[]args)
+    {
+        Scanner sc =new Scanner(System.in);
+        String h=sc.nextLine();
+        String m=sc.nextLine();
+        String s = sc.nextLine();
+        System.out.println(h+":"+m+":"+s);
+    }
+    }
+
